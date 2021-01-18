c3b279a1fd135c46e6cd16f8b40b2b6bc5180915
大石 光詩 <taikousi504@gmail.com>
2021-01-19 07:46:16 +0900

    敵追加

--- Game/Play.h
+++ Game/Play.h
@@ -4,10 +4,21 @@
 #include"LibMath.h"
 #include"Vector.h"
 #include"Player.h"
+#include"Enemy.h"
+#include<vector>
 class Play :public Scene
 {
 private:
 	Player* player[2];
+	std::vector<Enemy*> enemies;
+
+	//移動量をプレイヤーの向きに更新する間隔
+	//極端に少ないとクリア不可能になる
+	const int UPDATE_VELOCITY_TIME = 90;
+	const int ADD_ENEMY_TIME = 180;
+
+	int addEnemyTimer = 0;
+
 public:
 	Play();
 	~Play();
